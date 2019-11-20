a739623dcda2e29c58826d7cdf271bb97524c5bc
ofridesign <ofriki10@gmail.com>
2019-11-20 15:43:32 +0200

    new params

--- servo_util.h
+++ servo_util.h
@@ -28,8 +28,8 @@ MinMaxCategory speedCategories[numOfSpeedCategories] = {
 	MinMaxCategory(191, 220)};
 
 // Movement properties calculations
-int posFrom = 15;
-int posTo = 45;
+int posFrom = 100;
+int posTo = 130;
 
 /* 
  * DELAY DATA (The numbers are in Miliseconds)
